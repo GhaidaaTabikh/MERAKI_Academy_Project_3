55ef44c30128e4d6bda33f9b9b660b708686d4b0
Ghaidaa_tabikh <tabikh.ghaidaa@gmail.com>
2021-05-19 23:20:08 +0300

    createNewAuthor [Level 2]

--- schema.js
+++ schema.js
@@ -1,39 +1,41 @@
 //Starting step MongoDB
+const bcrypt = require("bcrypt");
 
 const mongoose = require("mongoose");
 
 const usersSchema = new mongoose.Schema({
   firstName: { type: String },
-  lastName: { type: String , unique : true },
+  lastName: { type: String, unique: true },
   age: { type: Number },
   country: { type: String },
-  email: { type: String , unique : true , required :true },
-  password: { type: String , required :true },
+  email: { type: String, unique: true, required: true },
+  password: { type: String, required: true },
 });
 
 const articlesSchema = new mongoose.Schema({
-  title: { type: String , unique : true},
+  title: { type: String, unique: true },
   description: { type: String },
   author: { type: mongoose.Schema.Types.ObjectId, ref: "users" },
-  comments: [ {type: mongoose.Schema.Types.ObjectId , ref :"comments"} ] 
-
+  comments: [{ type: mongoose.Schema.Types.ObjectId, ref: "comments" }],
 });
 
 //8. Comments Starting Steps (MongoDB)[2]
 
 const commentsSchema = new mongoose.Schema({
   comment: { type: String },
-commenter: { type: mongoose.Schema.Types.ObjectId ,ref :"users"}
-})
+  commenter: { type: mongoose.Schema.Types.ObjectId, ref: "users" },
+});
 
+usersSchema.pre("save", async function () {
+  this.email = this.email.toLowerCase();
+  const hashedPassword = await bcrypt.hash(this.password, 10);
+  this.password = hashedPassword;
+});
 
-
-const users = mongoose.model("users",usersSchema)
-const articles = mongoose.model("articles",articlesSchema)
-const comments  = mongoose.model("comments",commentsSchema)
-
-
+const users = mongoose.model("users", usersSchema);
+const articles = mongoose.model("articles", articlesSchema);
+const comments = mongoose.model("comments", commentsSchema);
 
 module.exports.users = users;
 module.exports.articles = articles;
-module.exports.comments = comments
+module.exports.comments = comments;
